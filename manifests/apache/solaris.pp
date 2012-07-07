ccb326990122b8d48515b1f99daed2d7a5c9dc8d
Geoff Davis <gadavis@ucsd.edu>
2012-07-07 09:07:25 -0700

    Add php5.load file on solaris

--- manifests/apache/solaris.pp
+++ manifests/apache/solaris.pp
@@ -3,4 +3,9 @@ class php::apache::solaris inherits php::apache::common {
     ensure   => 'installed',
     provider => 'pkgutil',
   }
+
+  file { '/opt/csw/apache2/etc/mods-available/php5.load':
+    ensure => present,
+    source => "puppet:///modules/${module_name}/httpd/php.load",
+  }
 }
